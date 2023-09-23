049f269d829d7d4ffe0c1d0bdd19a86e5d8fe655
Rupesh Mangalam <rupeshm.cse21@gmail.com>
2023-09-23 16:13:23 +0000

    added api endpoints

--- indra/src/components/Navbar.js
+++ indra/src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import '../styles/Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-brand" style={{ fontSize: '30px', marginRight: '70vh' }}>
-        <a href="/">INDRA</a>
+        <Link to="/">INDRA</Link> {/* Use Link for home */}
       </div>
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a href="/Predictions">Prediction</a>
+          <Link to="/Predictions/Dashboard">Prediction</Link> {/* Use Link for Predictions */}
         </li>
         <li className="nav-item">
           <a href="https://mint-echidna-492.notion.site/Cloud-Burst-Prediction-System-ensemble-ML-models-for-accurate-predictions-b068ea9138ca4f2da683b29940d3aa6f?pvs=4">Documentation</a>
